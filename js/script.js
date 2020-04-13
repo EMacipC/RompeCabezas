4ba95cfe8737c15de1fa4a86d4245c137932f7d5
EliasM <elias.macip.1995@gmail.com>
2020-04-13 09:33:23 -0500

    correccion

--- js/script.js
+++ js/script.js
@@ -2,11 +2,26 @@ var cell=document.getElementById("cell");
 var piezas=document.getElementById("piezas");
 var dialogo=document.getElementById("dialogo")
 var selectedPiece=null;
+var b=dialogo.children[2];
 
+document.addEventListener("dragstart", function(event){
+	event.dataTransfer.setData("Text", event.target.id);
+	console.log("drag start");
+});
+document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+});
+document.addEventListener("drop", function(event) {
+  event.preventDefault();
+  if ( event.target.className == "droptarget" ) {
+    var data = event.dataTransfer.getData("Text");
+    event.target.appendChild(document.getElementById(data));
+  }
+});
 document.onkeypress= keypress;
 crearTab();
 crearPiece();
-
+b.onclick=reinciar;
 function crearCeldas(width,height,position){
 	
 	let cas = document.createElement("div");
@@ -15,27 +30,52 @@ function crearCeldas(width,height,position){
 	cas.style.border="1px solid";
 	cas.dataset.position=position;
 	cas.onclick=clickcell;
+	cas.className="droptarget";
 	return cas;
 }
-function crearPie(width,height,piec){
+
+function crearPie(width,height,piec,position){
 	var cellElement=document.createElement("div");
 	var piece =document.createElement("img");
 
 	//Configurando la celda para la pieza dentro de el contenedor de piezas
 	cellElement.style.width=width;
 	cellElement.style.height= height;
+	cellElement.className="droptarget";
 
 	//configurar la pieza dentro del contenedor pieza
+	
 	piece.width=width;
 	piece.height=height;
 	piece.style.border="1px solid"
 	piece.src=piec.image;
 	piece.dataset.position=piec.position;
 	piece.onclick=clickPice;
+	piece.id=("drag"+position);
+	piece.draggable=true;
 
 	//mandar la pieza a la celda
 	cellElement.appendChild(piece);
 	return cellElement;
+}
+
+function GererarRan(){
+	var ver=[];
+	let contador=0;
+	for(let i=0;i<16;i++){
+		let provicional=0;
+		provicional=Math.floor(Math.random() * 16);
+		for(let j=0;j<16;j++){
+			if(provicional!=ver[j]||i==0){
+				contador++;
+			}
+		}
+		if(contador==16){
+			ver.push(provicional);
+		}else{i--;}
+		contador=0;
+	}
+	return ver;
 }
 
 function crearTab(){
@@ -56,11 +96,11 @@ function crearPiece(){
 	width/=4;
 	height/=4;
 	var pieces =generarPD();
-	for(let i=0;i<16;i++){
-		let pieceE=crearPie(width,height,pieces[i]);
-		addPiece(pieceE);
-	}
-
+	let ran= GererarRan();
+	for(let i=0; i<16;i++){
+		let pieceE=crearPie(width,height,pieces[ran[i]],i);
+		addPiece(pieceE);	
+	}	
 }
 function addCas(element){
 	cell.appendChild(element);
@@ -86,8 +126,8 @@ function clickPice(e){
 }
 function clickcell(e){
 	if(selectedPiece){
-		let cell=e.target;
-		cell.appendChild(selectedPiece);
+		let cel=e.target;
+		cel.appendChild(selectedPiece);
 	}
 	else{
 		console.log("seleccione una pieza");
@@ -104,12 +144,15 @@ function showDialogo(result){
 	var imgE=dialogo.children[0];
 	var textC=dialogo.children[1];
 	if(result){
-		imgE.src="img/ganas.jpg";
-		textC.innerText="Ganaste";
+		dialogo.style.background="green"
+		imgE.src="";
+		textC.innerText="¡¡Ganaste!!";
 	}
 	else{
-		imgE.src="img/perder.jpg";
-		textC.innerText="Perdiste";
+		dialogo.style.background="red"
+		imgE.src="";
+		textC.innerText="¡¡Perdiste!!";
+		
 	}
 	dialogo.style.display="block";
 }
@@ -125,11 +168,18 @@ function evaluateBoard(){
 	return true;
 }
 function returnPieces(){
+
 	let cells =cell.children;
 	let cellPieces= piezas.children;
-	for(cell of cells){
-		let position=cell.dataset.position;
-		let piece=cell.children[0];
-		cellPieces[piece.dataset.position].appendChild(piece);
+	let ran=GererarRan();
+	let i=-1;
+	for(cel of cells){
+		i++;
+		let piece=cel.children[0];
+		piezas.children[ran[i]].appendChild(piece);
 	}
+}
+function reinciar(){
+	dialogo.style.display="none";
+	returnPieces();
 }
